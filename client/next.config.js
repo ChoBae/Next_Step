c5ce31b30d93d8a1da383af742fdb81f085eaa98
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-03-22 03:03:55 +0900

    fix: nextConfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // pageDirectory: 'pages',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

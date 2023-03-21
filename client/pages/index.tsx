c93a6ebe35da56558d59e29b5777d94ed9d18e6c
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-03-22 02:55:54 +0900

    creat: NextStep!

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+// const inter = Inter({ subsets: ['latin'] })
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Next Step</h1>
+    </div>
+  );
+};
+export default Home;

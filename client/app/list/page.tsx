f500ddc5b4c2522424764f2d7a2baf3a95d5e8e8
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-04-22 15:49:33 +0900

    feat: 리스트 페이지 다이나믹 렌더링 설정

--- client/app/list/page.tsx
+++ client/app/list/page.tsx
@@ -1,6 +1,8 @@
 import { clientDB } from "@/util/database";
 import ListItem from "./components/ListItem";
 
+export const dynamic = "force-dynamic"; 
+
 export default async function List() {
   let client = await clientDB;
   let db = client.db("forum");

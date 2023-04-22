d610965aab0b2c4ac21a0efa38e5688360528b0a
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-04-22 16:48:25 +0900

    feat: 댓글 추가 시 바로 렌더링

--- client/app/detail/Commnet.tsx
+++ client/app/detail/Commnet.tsx
@@ -1,18 +1,37 @@
 "use client";
 
+import { useEffect, useState, useRef } from "react";
+
 interface CommentProps {
   postID: string;
 }
 
 export default function Comment(props: CommentProps) {
   // const [comment, setComment] = useState("");
+  const [commentList, setCommentList] = useState([]); // [
+  const formRef = useRef<HTMLFormElement>(null);
+  useEffect(() => {
+    fetch(`/api/comment/${props.postID}`)
+      .then((res) => {
+        console.log(res);
+        if (!res.ok)
+          throw Error("댓글 목록을 가져오는 중 오류가 발생했습니다.");
+        return res.json();
+      })
+      .then((data) => {
+        setCommentList(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("댓글 목록을 가져오는 중 오류가 발생했습니다.");
+      });
+  }, []);
 
   const commentSumbitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const comment = formData.get("comment");
 
-    // console.log(comment);
     fetch(`/api/comment/new`, {
       method: "POST",
       headers: {
@@ -24,11 +43,23 @@ export default function Comment(props: CommentProps) {
       }),
     })
       .then((res) => {
-        console.log(res);
         if (!res.ok) throw Error("댓글 등록 중 오류가 발생했습니다.");
         return res.json();
       })
-      .then(() => {})
+      .then(() => {
+        return fetch(`/api/comment/${props.postID}`);
+      })
+      .then((res) => {
+        if (!res.ok)
+          throw Error("댓글 목록을 가져오는 중 오류가 발생했습니다.");
+        return res.json();
+      })
+      .then((data) => {
+        setCommentList(data);
+        if (formRef.current) {
+          formRef.current.reset();
+        }
+      })
       .catch((error) => {
         console.error(error);
         alert("댓글 등록 중 오류가 발생했습니다.");
@@ -38,7 +69,15 @@ export default function Comment(props: CommentProps) {
   return (
     <div>
       <div>댓글목록</div>
-      <form onSubmit={commentSumbitHandler}>
+      {commentList.map((comment: any) => {
+        return (
+          <div key={comment._id}>
+            <p>{comment.comment}</p>
+            <p>{comment.author}</p>
+          </div>
+        );
+      })}
+      <form onSubmit={commentSumbitHandler} ref={formRef}>
         <input type="text" name="comment" />
         <button>댓글전송</button>
       </form>
